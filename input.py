9c4e0407e419dad8f73e54b9d69f112969e75398
MrValdez <shunyvaldez@gmail.com>
2018-01-26 23:12:29 +0800

    added movement

--- input.py
+++ input.py
@@ -3,6 +3,7 @@ import pygame
 class Input:
     def __init__(self):
         self.left = self.right = self.up = self.down = False
+        self.left_hold = self.right_hold = self.up_hold = self.down_hold = False
         self.button1 = self.button2 = False
         self.prev_keys = pygame.key.get_pressed()
 
@@ -22,6 +23,13 @@ class Input:
         self.down = check_key(pygame.K_DOWN)
         self.button1 = check_key(pygame.K_q)
         self.button2 = check_key(pygame.K_w)
+
+        self.left_hold = keys[pygame.K_LEFT]
+        self.right_hold = keys[pygame.K_RIGHT]
+        self.up_hold = keys[pygame.K_UP]
+        self.down_hold = keys[pygame.K_DOWN]
+        self.button1_hold = keys[pygame.K_q]
+        self.button2_hold = keys[pygame.K_w]
 
         self.prev_keys = keys
 
--- stages/stage1.py
+++ stages/stage1.py
@@ -1,3 +1,4 @@
+import pyganim
 from .stage import Stage, Text
 
 class Stage1(Stage):
@@ -6,6 +7,24 @@ class Stage1(Stage):
 
         self.texts = [Text("Remove interference", (255, 255, 255), self._center_text)]
 
+        self.sprite = pyganim.PygAnimation([("images/spam.png", 10)])
+        self.sprite.play()
+        self.sprite_size = self.sprite.getMaxSize()
+
+        self.pos = [0, 0]
+
+    def update(self, input):
+        speed = 10
+        if input.left_hold:  self.pos[0] -= speed
+        if input.right_hold: self.pos[0] += speed
+        if input.up_hold:    self.pos[1] -= speed
+        if input.down_hold:  self.pos[1] += speed
+        
+        self.pos[0] = max(0, min(self.pos[0], self.resolution[0] - self.sprite_size[0]))
+        self.pos[1] = max(0, min(self.pos[1], self.resolution[1] - self.sprite_size[1]))
+
     def draw(self, screen):
         screen.fill([255,128,128])
         super().draw(screen)
+
+        self.sprite.blit(screen, self.pos)
