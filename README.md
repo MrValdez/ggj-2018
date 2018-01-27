bf9d16aa0dac6dd2f30b415fb6e6b522d4e2f0b8
MrValdez <shunyvaldez@gmail.com>
2018-01-27 11:07:56 +0800

    added win condition

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 # art assets
 
-https://opengameart.org/content/clicker-game
-https://opengameart.org/content/wooden-fortress-and-animated-doors
-https://commons.wikimedia.org/wiki/File:Male_and_Female_USB_Connectors.jpg
+## Creative Commons
+https://opengameart.org/content/clicker-game  
+https://opengameart.org/content/wooden-fortress-and-animated-doors  
+https://commons.wikimedia.org/wiki/File:Male_and_Female_USB_Connectors.jpg  
+
+## Linkware License
+https://visualpharm.com/free-icons/sad%20cloud-595b40b85ba036ed117dc41c
+
+## Personally taken pictures
+usb ok.jpg
--- stages/stage7.py
+++ stages/stage7.py
@@ -15,7 +15,10 @@ class Stage7(Stage):
         self.female = pyganim.PygAnimation([("images/usb female.png", 10)])
         self.female.play()
 
-        self.yeah = pyganim.PygAnimation([("images/spam.png", 10)])
+        self.sad = pyganim.PygAnimation([("images/sad cloud.png", 10)])
+        self.sad.play()
+
+        self.yeah = pyganim.PygAnimation([("images/usb ok.jpg", 10)])
         self.yeah.play()
 
         self.reset()
@@ -24,35 +27,59 @@ class Stage7(Stage):
         self.texts = [Text("Connect Male and Female ends", (255, 255, 255), self._center_text)]
         self.flips = 0
         self.rotate = 0
-        self.victory = False
-
+        self.victory = 0
+        self.sad_visible = 0
+        self.distance = 200
+        
     def update(self, input, tick):
-        #self.victory = True
         if self.victory:
             self.texts = [Text("YEAHHHHHHHHH~!", (255, 128, 100), self._center_text)]
-            self.shake += tick
+            self.victory += tick
+            
+            if self.victory > 1500:
+                return True
+            return
 
-        #if self.shake > self.target_shake + 1000:
-        #    return True
         if input.button or input.up or input.down:
+            self.distance = 200
             self.male.flip(False, True)
+            self.sad_visible = 0
+            self.rotate += 1
+
+        if input.right:
+            self.distance = 40
+            
+            if self.rotate < 3:
+                self.rotate = 0
+                self.sad_visible = 1000
+            else:
+                self.distance = 100
+                self.victory = 1
+
+        self.sad_visible -= tick
 
     def draw(self, screen):
         super().draw(screen)
 
+        male_rect = self.male.getRect()
+        female_rect = self.female.getRect()
+
+        male_rect.centerx = (self.resolution[0] / 2) - self.distance
+        male_rect.centery = self.resolution[1] / 2
+        female_rect.centerx = (self.resolution[0] / 2) + self.distance
+        female_rect.centery = self.resolution[1] / 2
+
+        self.male.blit(screen, male_rect)
+        self.female.blit(screen, female_rect)
+
         if not self.victory:
-            male_rect = self.male.getRect()
-            female_rect = self.female.getRect()
-
-            male_rect.centerx = (self.resolution[0] / 2) - 100
-            male_rect.centery = self.resolution[1] / 2
-            female_rect.centerx = (self.resolution[0] / 2) + 100
-            female_rect.centery = self.resolution[1] / 2
-
-            self.male.blit(screen, male_rect)
-            self.female.blit(screen, female_rect)
+            rect = self.sad.getRect().move(self.resolution[0], 0)
+            if self.sad_visible > 0:
+                rect.centerx = self.resolution[0] / 2
+                rect.centery = self.resolution[1] / 2
+            self.sad.blit(screen, rect)
         else:
             rect = self.yeah.getRect()
             rect.centerx = self.resolution[0] / 2
-            rect.bottom = self.resolution[1]
+            rect.centery = self.resolution[1] / 2
             self.yeah.blit(screen, rect)
